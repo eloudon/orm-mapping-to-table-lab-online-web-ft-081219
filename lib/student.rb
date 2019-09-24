11dd611a9f5d0be5c147fb82ee656cebfc9c9394
Eric Loudon <loudon.eric@gmail.com>
2019-09-24 11:03:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,13 @@
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  
+
   def initialize(name, grade, id=nil)
       @id = id
       @name = name
       @grade = grade
     end
-  
+
   def self.create_table
       sql =  <<-SQL
         CREATE TABLE IF NOT EXISTS students (
@@ -18,25 +18,25 @@ class Student
           SQL
       DB[:conn].execute(sql)
   end
-  
+
   def self.drop_table
       sql =  <<-SQL
       DROP TABLE students
       SQL
     DB[:conn].execute(sql)
-  
+
   end
-  
+
   def save
         sql = <<-SQL
           INSERT INTO students (name, grade)
           VALUES (?, ?)
         SQL
-  
+
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-  
+
  def self.create(name:, grade:)
       student = Student.new(name, grade)
       student.save
